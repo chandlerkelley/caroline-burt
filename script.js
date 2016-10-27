6dcf98a7b598d3894e07c058a4c01a5ab9e1741e
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-27 15:44:34 -0400

    Added strobing title

--- script.js
+++ script.js
@@ -1,4 +1,22 @@
-function playSound(soundfile) {
-  // document.getElementById("click").innerHTML= "<embed src='audio/click.mp3' hidden='true' autostart='true' loop='false' />";
-  document.getElementById("click").innerHTML= "<embed src='audio/click.ogg' hidden='true' autostart='true' loop='false' />";
-}
+// function playSound(soundfile) {
+//   // document.getElementById("click").innerHTML= "<embed src='audio/click.mp3' hidden='true' autostart='true' loop='false' />";
+//   document.getElementById("click").innerHTML= "<embed src='audio/click.ogg' hidden='true' autostart='true' loop='false' />";
+// }
+
+$(function() {
+	
+	function changeTitle() {
+		switch($(".home-title").attr("src")) {
+		case "images/caroline-title.png":
+			$(".home-title").attr("src", "images/caroline-title-blue.png")
+			break;
+		case "images/caroline-title-blue.png":
+			$(".home-title").attr("src", "images/caroline-title.png")
+			break;
+		default:
+			console.log("Error")
+		}
+	};
+	
+	setInterval(changeTitle, 100);
+})
