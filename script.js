8720ef6a2875ea94d06e473d6a641028d73a52fd
Chandler Kelley <chandler.t.kelley@gmail.com>
2016-10-28 18:25:35 -0400

    Added a bunch of animations

--- script.js
+++ script.js
@@ -3,26 +3,26 @@
 //   document.getElementById("click").innerHTML= "<embed src='audio/click.ogg' hidden='true' autostart='true' loop='false' />";
 // }
 
-$(function() {
+// $(function() {
 	
-	function changeTitle() {
-		switch($(".home-title").attr("src")) {
-		case "images/caroline-title.png":
-			$(".home-title").attr("src", "images/caroline-title-blue.png")
-			break;
-		case "images/caroline-title-blue.png":
-			$(".home-title").attr("src", "images/caroline-title-green.png")
-			break;
-		case "images/caroline-title-green.png":
-			$(".home-title").attr("src", "images/caroline-title-gold.png")
-			break;
-		case "images/caroline-title-gold.png":
-			$(".home-title").attr("src", "images/caroline-title.png")
-			break;
-		default:
-			console.log("Error")
-		}
-	};
+// 	function changeTitle() {
+// 		switch($(".home-title").attr("src")) {
+// 		case "images/caroline-title.png":
+// 			$(".home-title").attr("src", "images/caroline-title-blue.png")
+// 			break;
+// 		case "images/caroline-title-blue.png":
+// 			$(".home-title").attr("src", "images/caroline-title-green.png")
+// 			break;
+// 		case "images/caroline-title-green.png":
+// 			$(".home-title").attr("src", "images/caroline-title-gold.png")
+// 			break;
+// 		case "images/caroline-title-gold.png":
+// 			$(".home-title").attr("src", "images/caroline-title.png")
+// 			break;
+// 		default:
+// 			console.log("Error")
+// 		}
+// 	};
 	
-	setInterval(changeTitle, 160);
-})
+// 	setInterval(changeTitle, 160);
+// })
